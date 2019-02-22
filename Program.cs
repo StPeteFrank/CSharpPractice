43227af601b394540944f3cc05dfdd41d9019d43
Frank Diaz <diazfrankp@gmail.com>
2019-02-22 14:28:35 -0500

    Basic array complete.

--- Program.cs
+++ Program.cs
@@ -20,11 +20,39 @@ namespace csharppractice
     }
     static void Arrays()
     {
-      Console.WriteLine("testing");
+      int[] myNumbers = { 2, 4, 6, 7, 9, 11, 23 };
+      string[] friends = new string[10];
+      string[] girlfriends = new string[10];
+
+      friends[0] = "Alex";
+      friends[1] = "Ami";
+      friends[2] = "Joe";
+
+      girlfriends[0] = "Nikki";
+      girlfriends[1] = "Hot Stripper";
+
+
+      Console.WriteLine("Today " + friends[0] + " broke up with his girlfriend " + girlfriends[0] + ".");
+      Console.WriteLine(friends[1] + " and " + friends[2] + " were very supportive of " + friends[0]
+      + " and bought him " + myNumbers[0] + " " + girlfriends[1] + "s.");
+
       Console.ReadLine();
     }
+
+    // static void Methods();
+    // {
+
+    // }
+
   }
 }
+
+
+
+
+
+
+
 // Console.WriteLine("Hello World");
 //     CreateWebHostBuilder(args).Build().Run();
 //   }
