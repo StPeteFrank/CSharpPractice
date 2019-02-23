bd975439639c2fbcb57f5d9bd5ff6fcf6e268f22
Frank Diaz <diazfrankp@gmail.com>
2019-02-22 19:33:18 -0500

    Second calculatot complete.

--- Program.cs
+++ Program.cs
@@ -18,32 +18,62 @@ namespace csharppractice
       Console.Write("Enter a number: ");
       double num1 = Convert.ToDouble(Console.ReadLine());
 
-      Console.Write("Enter Operator: ");
+      Console.Write("Enter an operator: ");
       string op = Console.ReadLine();
 
       Console.Write("Enter a number: ");
       double num2 = Convert.ToDouble(Console.ReadLine());
 
-      if (op == "+")
+      if (op == "*")
       {
-        Console.WriteLine(num1 + num2);
-      }
-      else if (op == "-")
-      {
-        Console.WriteLine(num1 - num2);
+        Console.WriteLine(num1 * num2);
       }
       else if (op == "/")
       {
         Console.WriteLine(num1 / num2);
       }
-      else if (op == "*")
+      else if (op == "-")
       {
-        Console.WriteLine(num1 * num2);
+        Console.WriteLine(num1 - num2);
+      }
+      else if (op == "+")
+      {
+        Console.WriteLine(num1 + num2);
       }
       else
       {
-        Console.WriteLine("Invalid Operator");
+        Console.WriteLine("You entered an incorrect operator!");
       }
+
+      // Console.Write("Enter a number: ");
+      // double num1 = Convert.ToDouble(Console.ReadLine());
+
+      // Console.Write("Enter Operator: ");
+      // string op = Console.ReadLine();
+
+      // Console.Write("Enter a number: ");
+      // double num2 = Convert.ToDouble(Console.ReadLine());
+
+      // if (op == "+")
+      // {
+      //   Console.WriteLine(num1 + num2);
+      // }
+      // else if (op == "-")
+      // {
+      //   Console.WriteLine(num1 - num2);
+      // }
+      // else if (op == "/")
+      // {
+      //   Console.WriteLine(num1 / num2);
+      // }
+      // else if (op == "*")
+      // {
+      //   Console.WriteLine(num1 * num2);
+      // }
+      // else
+      // {
+      //   Console.WriteLine("Invalid Operator");
+      // }
 
       // Console.WriteLine(GetMax(20, 30, 50));
 
