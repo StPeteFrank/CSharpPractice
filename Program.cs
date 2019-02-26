04adf741aebdffb8c691afb9cd27976ef1253a0b
Frank Diaz <diazfrankp@gmail.com>
2019-02-26 09:39:36 -0500

    Creating multiple books.

--- Program.cs
+++ Program.cs
@@ -23,6 +23,19 @@ namespace csharppractice
       book1.author = "JK Rowlings";
       book1.pages = 400;
 
+      Book book2 = new Book();
+      book2.title = "LOTR";
+      book2.author = "Tolkien";
+      book2.pages = 700;
+
+      Console.WriteLine(book1.title);
+      Console.WriteLine(book1.author);
+      Console.WriteLine(book1.pages);
+
+      Console.WriteLine(book2.title);
+      Console.WriteLine(book2.author);
+      Console.WriteLine(book2.pages);
+
       ///
 
       // try
