e4f48bfa8d87cd549ef8cc0b604108a8a2127714
Frank Diaz <diazfrankp@gmail.com>
2019-02-27 07:55:28 -0500

    Challenge Disemvowel.

--- Program.cs
+++ Program.cs
@@ -757,6 +757,38 @@ namespace csharppractice
 
     ///////////////////////////////////////////////////////
 
+    // Your task is to write a function that takes a string and 
+    // return a new string with all vowels removed.
+
+    // For example, the string "This website is for losers LOL!" would become 
+    // "Ths wbst s fr lsrs LL!".
+
+    // Note: for this kata y isn't considered a vowel.
+
+    public static class Kata3
+    {
+      public static string Disemvowel(string str)
+      {
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+        return string.Concat(str.Split(vowels));
+      }
+    }
+    //
+    public static class Kata4
+    {
+      public static string Disemvowel(string str)
+      {
+        string[] vowels = { "a", "e", "i", "o", "u", "A", "E", "I", "O", "U" };
+        for (int i = 0; i < vowels.Length; i++)
+        {
+          str = str.Replace(vowels[i], "");
+        }
+        return str;
+      }
+    }
+
+    ///////////////////////////////////////////////////////
+
   }
 }
 
