0155c6da605d98a718cee67ac1b2d65e43d867bc
Frank Diaz <diazfrankp@gmail.com>
2019-02-27 08:12:36 -0500

    Challenge ListFiltered.

--- Program.cs
+++ Program.cs
@@ -800,6 +800,18 @@ namespace csharppractice
         return Convert.ToString(a + b, 2);
       }
     }
+    //////////////////////////////////////////////////////
+
+    // In this kata you will create a function that takes a list of non-negative integers 
+    // and strings and returns a new list with the strings filtered out.
+
+    public class ListFilterer
+    {
+      public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+      {
+        return listOfItems.OfType<int>();
+      }
+    }
 
 
 
