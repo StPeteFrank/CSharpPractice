e17dcffa080562e786a454b88f6cc26a13c93def
Frank Diaz <diazfrankp@gmail.com>
2019-02-28 10:29:53 -0500

    Created RecipeBooks class.

---
+++ Controllers/Classes/RecipeBooks.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeBooks
+{
+  class RecipeBook
+  {
+    public string title;
+    public string author;
+    public int pages;
+    public int numberOfRecipes;
+  }
+}
