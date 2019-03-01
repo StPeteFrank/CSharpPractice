639852545b0cd402913dd86b4060cc2fa901585c
Frank Diaz <diazfrankp@gmail.com>
2019-03-01 09:43:47 -0500

    Created Movies class with Movie constructor Get and Set.

---
+++ Controllers/Classes/Movies.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movies
+{
+  class Movie
+  {
+    public string title;
+    public string director;
+
+    public string rating;
+
+    public Movie(string aTitle, string aDirector, string aRating)
+    {
+      title = aTitle;
+      director = aDirector;
+      rating = aRating;
+    }
+  }
+}
