b18eec16e6dd5b6641ee238de253e60cf8ffa260
Frank Diaz <diazfrankp@gmail.com>
2019-03-01 09:59:05 -0500

    Added get and set to rating attribute.

--- Controllers/Classes/Movies.cs
+++ Controllers/Classes/Movies.cs
@@ -10,14 +10,30 @@ namespace csharppractice.Movies
   {
     public string title;
     public string director;
-
-    public string rating;
-
+    private string rating;
     public Movie(string aTitle, string aDirector, string aRating)
     {
       title = aTitle;
       director = aDirector;
       rating = aRating;
     }
+    public string Rating
+    {
+      get { return rating; }
+      set
+      {
+        if (value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR")
+        {
+          rating = value;
+        }
+        else
+        {
+          rating = "NR";
+        }
+      }
+    }
+
+
+    // properties is like a special method
   }
 }
