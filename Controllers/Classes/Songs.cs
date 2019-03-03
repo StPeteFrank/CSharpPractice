f216842c23603aa8dace61e01607f199789ef28e
Frank Diaz <diazfrankp@gmail.com>
2019-03-03 10:05:30 -0500

    Song constructor built.

--- Controllers/Classes/Songs.cs
+++ Controllers/Classes/Songs.cs
@@ -11,5 +11,14 @@ namespace Songs
     public string title;
     public string artist;
     public int duration;
+    public static int songCount = 0;
+    public Song(string aTitle, string aArtist, int aDuration)
+    {
+      title = aTitle;
+      artist = aArtist;
+      duration = aDuration;
+      songCount++;
+    }
   }
+
 }
